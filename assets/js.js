72d77bff4adb1694a41c3b78f6277c96de885740
faustluck <faustluck91@gmail.com>
2022-04-08 01:26:59 +0300

    master

--- assets/js.js
+++ assets/js.js
@@ -1,5 +1,5 @@
 function toggleTheme() {
-    let link = document.getElementsByTagName(link);
+    let link = document.getElementsByTagName('link');
     let light = a.filter(e => e.href.indexOf('light') > -1)[0]
     let dark = a.filter(e => e.href.indexOf('dark') > -1)[0]
 
@@ -9,14 +9,18 @@ function toggleTheme() {
     //window.matchMedia('(prefers-color-scheme: dark)')
 }
 
+function openMenu() {
+    document.addEventListener('click', function (e) {
+        if (window.matchMedia('(min-width: 768px)').matches) return;
+        let menu = document.querySelector('.menu');
+        if (e.target.closest('.menu:not(.open)')) {
+            menu.classList.add('open');
+            document.body.style.overflow = 'hidden';
+        } else if (e.target.closest('header')) {
+            menu.classList.remove('open');
+            document.body.style.overflow = 'auto';
+        }
+    })
+}
 
-document.addEventListener('click', function (e) {
-    let menu = document.querySelector('.menu');
-    if (e.target.closest('.menu:not(.open)')) {
-        menu.classList.add('open');
-        document.body.style.overflow = 'hidden';
-    } else if (e.target.closest('header')) {
-        menu.classList.remove('open');
-        document.body.style.overflow = 'auto';
-    }
-})
+openMenu()
