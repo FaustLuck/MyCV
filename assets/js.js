cf1010560346d77bc4d1be3c35a5564c7f4935db
faustluck <faustluck91@gmail.com>
2022-04-09 19:36:00 +0300

    master

--- assets/js.js
+++ assets/js.js
@@ -1,22 +1,3 @@
-function toggleTheme(e) {
-  if (!e.target.closest('.theme')) return;
-  let isDark = window.localStorage.getItem('dark')
-  isDark = (isDark == 'true') ? false : true;
-  window.localStorage.setItem('dark', isDark)
-  document.body.classList.toggle('dark')
-  document.body.classList.toggle('light')
-  toggleIcon()
-}
-
-function toggleMenu(e) {
-  if (window.matchMedia('(min-width: 768px)').matches) return;
-  if (!e.target.closest('header')) return
-  let menu = document.querySelector('.menu');
-  menu.classList.toggle('open');
-  let flag = Array.from(menu.classList).includes('open');
-  document.body.style.overflow = (flag) ? 'hidden' : 'auto'
-}
-
 function checkTheme() {
   let isDark = window.localStorage.getItem('dark');
   if (isDark == 'false' || isDark == null) {
@@ -29,14 +10,22 @@ function checkTheme() {
   toggleIcon()
 }
 
+function toggleTheme(e) {
+  if (!e.target.closest('.theme')) return;
+  let isDark = window.localStorage.getItem('dark')
+  isDark = (isDark == 'true') ? false : true;
+  window.localStorage.setItem('dark', isDark)
+  document.body.classList.toggle('dark')
+  document.body.classList.toggle('light')
+  toggleIcon()
+}
+
 function toggleIcon() {
   let icons = document.querySelectorAll('.icon');
   icons = Array.from(icons);
   icons = icons.map(toggleSrc)
-  //icons = icons.forEach(toggleIcon)
-
 }
-//TODO не меняет src
+
 function toggleSrc(el) {
   let sep = (window.localStorage.getItem('dark') == 'true') ? 'dark' : 'light';
   let newSep = (window.localStorage.getItem('dark') == 'true') ? 'light' : 'dark';
@@ -45,7 +34,41 @@ function toggleSrc(el) {
   el.src = tmp
 }
 
+function toggleMenu(e) {
+  if (window.matchMedia('(min-width: 768px)').matches) return;
+  let menu = document.querySelector('.menu');
+  if (e.target.closest('.writing')) menu.classList.remove('open')
+  if (!(e.target.closest('.menu') || e.target.closest('.overley:not(form'))) return
+  menu.classList.toggle('open');
+  toggleOverley()
+}
+
+function showHideOverflow(flag) {
+  document.body.style.overflow = (flag) ? 'hidden' : 'auto'
+}
+
+function toggleOverley(form = '') {
+  if (form) form = `.${form}`
+  let overley = document.querySelector(`.overley${form}`);
+  overley.classList.toggle('open')
+  let flag = overley.classList.contains('open')
+  showHideOverflow(flag)
+}
+
+function openForm(e) {
+  if (!(e.target.closest('.writing') || e.target.closest('.close'))) return;
+  let writingIcon = document.querySelector('.writing')
+  writingIcon.classList.toggle('hide')
+  if (document.querySelector('.overley').classList.contains('open')) toggleOverley()
+  toggleOverley('form');
+  let form = document.querySelector('.popup');
+  form.classList.toggle('open')
+}
+
+
+
 
 document.addEventListener('click', toggleMenu)
 document.addEventListener('click', toggleTheme)
 document.addEventListener('DOMContentLoaded', checkTheme)
+document.addEventListener('click', openForm)
